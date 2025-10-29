3d34906735248f0d1a608060adc49b65c3d22989
hrithik singh chandel <hrithik@worksemail.com>
2025-10-29 02:05:47 -0400

    code

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("hello")
+import cv2
+
+camera = cv2.VideoCapture(0)
+
+if not camera.isOpened():
+    print("canera not detected")
+else:
+    while True:
+        ret, frame =camera.read()
+    if not ret:
+        print("no frame recorded")
+        break
+    cv2.imshow("recording", frame)
+    if cv2.waitkey(1) & 0xFF == ord('q'):
+        break
+camera.release()
+cv2.destroyAllWindows()
